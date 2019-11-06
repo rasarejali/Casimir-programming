7f195684236e8483db27d08b6a5488365de61ae9
Rasa Rejali <rasa.rejali@mail.mcgill.ca>
2019-11-06 12:24:43 +0100

    calls on test.py to calculate circumference and surface area

--- script.py
+++ script.py
@@ -1,1 +1,4 @@
+import test
 print('Hello world!')  
+print(test.circumference(10))
+print(test.surfacearea(10))
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import numpy as np
-print('Hello world!')
+
 def circumference(r):
 	return 2*np.pi*r
 def surfacearea(r):
